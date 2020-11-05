9215b946489652a1430ca3cd139cc0dd626c0ebe
Stephanie Kwan <skwan@princeton.edu>
2020-11-05 22:20:05 +0100

    rebase cms-nanoAOD-master

--- python/postprocessing/examples/example_postproc.py
+++ python/postprocessing/examples/example_postproc.py
@@ -18,10 +18,15 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 impor
 
 jmeCorrections = createJMECorrector(True, "2018", "B", "Total", False, "AK4PFchs", False)
 
+##Function parameters
+##(isMC=True, dataYear=2016, runPeriod="B", jesUncert="Total", redojec=False, jetType = "AK4PFchs", noGroom=False)
+##All other parameters will be set in the helper module
+
+jmeCorrections = createJMECorrector(True, "2018", "B", "Total", False, "AK4PFchs", False)
+
 #fnames=["/eos/cms/store/mc/RunIISummer16NanoAODv5/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PUMoriond17_Nano1June2019_102X_mcRun2_asymptotic_v7_ext2-v1/120000/FF69DF6E-2494-F543-95BF-F919B911CD23.root"]
 fnames=['/eos/user/s/skkwan/hToAA/syncNanoAOD/102X_RunIIAutumn18/VBFHToTauTau.root']
 
 #p=PostProcessor(".",fnames,"Jet_pt>150","",[jetmetUncertainties2016(),exampleModuleConstr()],provenance=True)
 p=PostProcessor(".",fnames,"","keep_and_drop.txt",[jmeCorrections()],provenance=True)
-
 p.run()
