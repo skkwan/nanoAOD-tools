879cda2d79e684feec39c0ae4011fdff8e816a02
Stephanie Kwan <skwan@princeton.edu>
2021-05-24 14:19:45 -0400

    work in progress, stash changes

--- python/postprocessing/sync2018/farmout_postprocTemplate.py
+++ python/postprocessing/sync2018/farmout_postprocTemplate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os, sys
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
@@ -11,29 +12,36 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 impor
 
 # Function parameters
 
-jmeCorrections = createJMECorrector(isMC=True, dataYear=2018, runPeriod="B",
-                                    jesUncert="Total", 
-                                    jetType="AK4PFchs",
-                                    noGroom="True",
-                                    applyHEMfix=True,
-                                    splitJER=False,
-                                    metBranchName="MET")
+def main(argv=None):
+    jmeCorrections = createJMECorrector(isMC=True, dataYear=2018, runPeriod="B",
+                                        jesUncert="Total", 
+                                        jetType="AK4PFchs",
+                                        noGroom="True",
+                                        applyHEMfix=True,
+                                        splitJER=False,
+                                        metBranchName="MET")
 
-# farmout's inputFileNames will substitute with "1.root", "2.root" so no parentheses are needed
-stems=[$inputFileNames]
-fnames=[]
+    fnames=[os.getenv('INPUT')]
+    
+    for f in fnames:
+        print("Printing file names...")
+        print(f)
 
-for f in stems:
-    fnames.append("root://cmsxrootd.fnal.gov/" + f)
+        # p=PostProcessor(".",
+        #                 ["/hdfs/store/user/skkwan/ACDEB11A-E534-5043-899F-9880E41725DC.root"], 
+        #                 cut="(nMuon > 0) && (nTau > 0)",
+        #                 branchsel="keep_and_drop_input.txt",
+        #                 outputbranchsel="keep_and_drop_output.txt",
+        #                 maxEntries=10,
+        #                 modules=[jmeCorrections()],
+        #                 postfix="_postproc",
+        #                 provenance=True)
+        
+        # p.run()
+        
+        
 
-p=PostProcessor(".",
-                ["/hdfs/store/user/skkwan/ACDEB11A-E534-5043-899F-9880E41725DC.root"], 
-                cut="(nMuon > 0) && (nTau > 0)",
-                branchsel="keep_and_drop_input.txt",
-                outputbranchsel="keep_and_drop_output.txt",
-                maxEntries=10,
-                modules=[jmeCorrections()],
-                postfix="_postproc",
-                provenance=True)
 
-p.run()
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
---
+++ python/postprocessing/sync2018/runJmeSubmitter.sh
@@ -0,0 +1,4 @@
+# Run jmeSubmitter.py
+
+jobname="testlocal"
+python jmeSubmitter.py --sampledir VBFHToTauTau --jobName ${jobname} --inFile test_local_input.txt 
