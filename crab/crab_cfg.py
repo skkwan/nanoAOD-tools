731d82db04243e2c331f00c77d7f6a8679b8c8bc
Stephanie Kwan <skwan@princeton.edu>
2021-09-23 15:50:52 -0500

    Minimal working example on VBFHToTauTau sync sample

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -1,11 +1,13 @@
 from WMCore.Configuration import Configuration
-from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+from CRABClient.UserUtilities import config
 
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'NanoPost1'
+config.General.workArea = 'crab_projects'
+config.General.requestName = 'NanoPost_test'
 config.General.transferLogs = True
+
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
@@ -13,22 +15,21 @@ config.JobType.scriptExe = 'crab_script.sh'
 # hadd nano will not be needed once nano tools are in cmssw
 config.JobType.inputFiles = ['crab_script.py', '../scripts/haddnano.py']
 config.JobType.sendPythonFolder = True
+
 config.section_("Data")
-config.Data.inputDataset = '/DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17NanoAOD-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/NANOAODSIM'
+config.Data.inputDataset = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIIAutumn18NanoAODv7-Nano02Apr2020_102X_upgrade2018_realistic_v21_ext1-v1/NANOAODSIM'
 #config.Data.inputDBS = 'phys03'
 config.Data.inputDBS = 'global'
+#config.Data.splitting = 'Automatic'
 config.Data.splitting = 'FileBased'
 #config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 2
-config.Data.totalUnits = 10
+config.Data.unitsPerJob = 1
+#config.Data.totalUnits = 10
 
-config.Data.outLFNDirBase = '/store/user/%s/NanoPost' % (
-    getUsernameFromSiteDB())
-config.Data.publication = False
-config.Data.outputDatasetTag = 'NanoTestPost'
+config.Data.outLFNDirBase = '/store/user/skkwan/NanoPost'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'NanoPost_Test_VBFHToTauTau'
+
 config.section_("Site")
-config.Site.storageSite = "T2_DE_DESY"
+config.Site.storageSite = "T2_US_Wisconsin"
 
-#config.Site.storageSite = "T2_CH_CERN"
-# config.section_("User")
-#config.User.voGroup = 'dcms'
