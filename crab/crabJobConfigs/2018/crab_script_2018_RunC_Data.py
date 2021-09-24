76e56ff0a40e1678ddc1a4a91582ec297b7b60bc
Stephanie Kwan <skwan@princeton.edu>
2021-09-24 14:13:33 -0500

    add 2018 Data .sh and .py for crab jobs

---
+++ crab/crabJobConfigs/2018/crab_script_2018_RunC_Data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+from importlib import import_module
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
+##soon to be deprecated                                                                                
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import *
+##new way of using jme uncertainty                                                                  
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import *
+# this takes care of converting the input files from CRAB
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles, runsAndLumis
+
+jmeCorrections = createJMECorrector(isMC=False, dataYear=2018, runPeriod="C",
+                                    jesUncert="Total",
+                                    jetType="AK4PFchs",
+                                    noGroom="True",
+                                    applyHEMfix=True,
+                                    splitJER=False,
+                                    metBranchName="MET")
+
+p = PostProcessor(".",
+                  inputFiles(),
+                  modules=[jmeCorrections()],
+                  provenance=True,
+                  fwkJobReport=True,
+                  jsonInput=runsAndLumis())
+
+p.run()
+
+print("DONE")
