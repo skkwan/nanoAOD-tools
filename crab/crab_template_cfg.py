9643536ceabc5a4eb0822dd0fea92b31e892bde0
Stephanie Kwan <skwan@princeton.edu>
2024-03-04 13:03:19 -0600

    add latest scripts

--- crab/crab_template_cfg.py
+++ crab/crab_template_cfg.py
@@ -19,14 +19,14 @@ config.JobType.scriptExe  = 'CRAB_BASH_SCRIPT'
 # config.JobType.scriptArgs = ['script=CRAB_SCRIPT_PYTHON_NAME']
 # hadd nano will not be needed once nano tools are in cmssw
 config.JobType.inputFiles = ['CRAB_PYTHON_SCRIPT_PATH', '../../../scripts/haddnano.py']
-config.JobType.sendPythonFolder = True
+#config.JobType.sendPythonFolder = True
 
 config.section_("Data")
 config.Data.inputDataset = 'DAS_NAME'
 config.Data.inputDBS     = 'INPUT_DBS'
-#config.Data.splitting    = 'FileBased'
-#config.Data.unitsPerJob  = 1
-config.Data.splitting    = 'Automatic'
+config.Data.splitting    = 'FileBased'
+config.Data.unitsPerJob  = 1
+#config.Data.splitting    = 'Automatic'
 
 config.Data.outLFNDirBase    = '/store/user/skkwan/NanoPost'
 config.Data.publication      = True
