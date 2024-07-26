cd5a36079d039c4ceb041929ee1ccf15e6eb76b3
Stephanie Kwan <skwan@princeton.edu>
2024-07-26 09:33:52 -0500

    update script

--- crab/makeCrabReport.py
+++ crab/makeCrabReport.py
@@ -14,9 +14,11 @@ for eraType in ["mc_2018"]:
 #for eraType in ["embed_2018", "data_2018_A", "data_2018_B", "data_2018_C", "data_2018_D"]:
    for name in config[eraType]["datasets"]:
       # Report
-      command = "echo '-------- {} ----------------------------- ' | tee -a {}".format(name, reportname)
+      command = f"echo '-------- {name} ----------------------------- ' | tee -a {reportname}"
       os.system(command)
-      command = "crab status -d crabJobConfigs/{}/crab_projects/crab_NanoPost_{}_{} | tee -a {}".format(year, name, config[eraType]["prodtag"], reportname)
+      command = f'crab status -d crab_projects/crab_NanoPost_{name}_{config[eraType]["prodtag"]} | tee -a {reportname}'
       print(command)
       os.system(command)
-      os.system("echo '\n\n' | tee -a {}".format(reportname))
+      os.system(f"echo '\n\n' | tee -a {reportname}")
+
+# crab_NanoPost_Cascade_GluGluH125toA1A2to3A1_A1to2Bor2Tau_MA1-15_MA2-70_RunIISummer20UL18NanoAODv9
