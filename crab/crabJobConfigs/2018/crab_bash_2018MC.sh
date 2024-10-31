001560ef97368cd97180c693deb704adee9e11bf
Stephanie Kwan <skwan@princeton.edu>
2024-10-31 08:49:17 -0500

    fix rm/mv that was causing CRAB jobs to fail late October, remove unused files

--- crab/crabJobConfigs/2018/crab_bash_2018MC.sh
+++ crab/crabJobConfigs/2018/crab_bash_2018MC.sh
@@ -13,14 +13,6 @@ echo "CMSSW BASE, python path, pwd"
 echo $CMSSW_BASE 
 echo $PYTHON_PATH
 echo $PWD 
-rm -rf $CMSSW_BASE/lib/
-rm -rf $CMSSW_BASE/src/
-rm -rf $CMSSW_BASE/module/
-rm -rf $CMSSW_BASE/python/
-mv lib $CMSSW_BASE/lib
-mv src $CMSSW_BASE/src
-mv module $CMSSW_BASE/module
-mv python $CMSSW_BASE/python
 
 echo Found Proxy in: $X509_USER_PROXY
 echo First argument is $1
